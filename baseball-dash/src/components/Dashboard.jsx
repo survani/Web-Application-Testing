74183848d29b0762402e7fa992986cb9fa117ace
survani <giovanicode@outlook.com>
2020-01-23 16:21:44 -0800

    Added some todos to organize my work and start

--- baseball-dash/src/components/Dashboard.jsx
+++ baseball-dash/src/components/Dashboard.jsx
@@ -1,16 +1,20 @@
+import React, { useState } from "react";
+
+const Dashboard = () => {
+//TODO for Dashboard:
+
+//#1 - states to include for [strikes, balls, fouls]
+//#2 - If logics to make rules for strikes, balls, and even fouls.
+//#3 - Create buttons w/ onClicks to add to the count.
+
+  return <div>This is the Dashboard component</div>;
+};
+
+export default Dashboard;
+
 /*TODO - <button/> that let the user press to count the following:
 stike & ball & foul, hit */
 
 //TODO - keep it simple no need for hit types.
 
 //TODO - Whatever is updating here should reflect into the Display component.
-
-import React from "react";
-
-const Dashboard = () => {
-    return (
-        <div>This is the Dashboard component</div>
-    )
-}
-
-export default Dashboard;
