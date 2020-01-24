503bd9ec6634932862755b1c631d1b201e24fc43
survani <giovanicode@outlook.com>
2020-01-23 16:00:35 -0800

    Added the components needed to start work

---
+++ baseball-dash/src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+/*TODO - <button/> that let the user press to count the following:
+stike & ball & foul, hit */
+
+//TODO - keep it simple no need for hit types.
+
+//TODO - Whatever is updating here should reflect into the Display component.
+
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div>This is the Dashboard component</div>
+    )
+}
+
+export default Dashboard;
